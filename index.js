86b850e4a2aaa6943fa03b805f61193fb9bbe50f
Abdul Manaaf <nafplann@gmail.com>
2017-11-14 12:44:48 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const ParseDashboard = require('parse-dashboard');
+const fs = require('fs');
+const port = 4040;
+const options = {};
+
+const dashboard = new ParseDashboard({
+    apps: [
+        {
+            'serverURL': 'http://SERVER_URL:1337/parse',
+            'appId': 'lorienAnalyticsAppId',
+            'masterKey': 'lorienAnalyticsMasterKey',
+            'appName': 'Lorien Analytics',
+            'iconName': 'lorien.jpg'
+        }
+    ],
+    users: [
+        { 'user': 'USER', 'pass': 'PASS' }
+    ],
+    iconsFolder: 'icons'
+}, true);
+
+const app = express();
+app.use('/', dashboard);
+
+const httpServer = require('http').createServer(app);
+httpServer.listen(port, () => {
+    console.log('parse-dashboard-starter is running on port ', port);
+});
